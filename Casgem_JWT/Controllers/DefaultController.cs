72f062908d44c6a00fef1324e719a021dadd6bb5
aleyna çelik <celikaleyna71@gmail.com>
2023-08-01 14:18:08 +0300

    Add project files.

---
+++ Casgem_JWT/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using Casgem_JWT.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Casgem_JWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        
+        [HttpGet("[Action]")]
+        public IActionResult Login()
+        {
+
+            return Created("", new BuildToken().CreateToken());
+        }
+        [Authorize]
+        [HttpGet("[Action]")]
+        public IActionResult CustomerList()
+        {
+
+            return Ok("Müşteri Listesine Eriştiniz");
+        }
+    }
+}
---
+++ Casgem_JWT/Models/BuildToken.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace Casgem_JWT.Models
+{
+    public class BuildToken
+    {
+        public string CreateToken()
+        {
+            var bytes = Encoding.UTF8.GetBytes("casgemprojetests");
+            SymmetricSecurityKey key =new SymmetricSecurityKey(bytes);
+
+           SigningCredentials credantials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            JwtSecurityToken token = new JwtSecurityToken(issuer: "http://localhost", audience: "http://localhost", notBefore: DateTime.Now, expires: DateTime.Now.AddMinutes(1), signingCredentials: credantials);
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            return handler.WriteToken(token);
+        }
+    }
+}
